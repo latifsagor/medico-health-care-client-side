5e3989bcf0bfbda42c0eedb50c46723d9efc1704
latifsagor <latif.sagor1042@gmail.com>
2021-10-18 03:36:12 +0600

    json upload

---
+++ src/Components/Home/Services/Services.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import './Services.css'
+
+const services = [
+  {
+    id: 1,
+    name: 'Ginecology',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer adipiscing erat eget risus sollicitudin pellentesque et non erat. Maecenas nibh dolor, malesuada et bibendum a, sagittis accumsan ipsum.',
+    img: 'https://i.ibb.co/bRqcMtp/01.png',
+  },
+  {
+    id: 2,
+    name: 'Ginecology',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer adipiscing erat eget risus sollicitudin pellentesque et non erat. Maecenas nibh dolor, malesuada et bibendum a, sagittis accumsan ipsum.',
+    img: 'https://i.ibb.co/YP7fSG5/02.png',
+  },
+  {
+    id: 3,
+    name: 'Ginecology',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer adipiscing erat eget risus sollicitudin pellentesque et non erat. Maecenas nibh dolor, malesuada et bibendum a, sagittis accumsan ipsum.',
+    img: 'https://i.ibb.co/3dTv2GX/03.png',
+  },
+  {
+    id: 4,
+    name: 'Ginecology',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer adipiscing erat eget risus sollicitudin pellentesque et non erat. Maecenas nibh dolor, malesuada et bibendum a, sagittis accumsan ipsum.',
+    img: 'https://i.ibb.co/VjY6bXh/04.png',
+  },
+  {
+    id: 5,
+    name: 'Ginecology',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer adipiscing erat eget risus sollicitudin pellentesque et non erat. Maecenas nibh dolor, malesuada et bibendum a, sagittis accumsan ipsum.',
+    img: 'https://i.ibb.co/DfksbR7/05.png',
+  },
+  {
+    id: 6,
+    name: 'Ginecology',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer adipiscing erat eget risus sollicitudin pellentesque et non erat. Maecenas nibh dolor, malesuada et bibendum a, sagittis accumsan ipsum.',
+    img: 'https://i.ibb.co/YLZn82L/06.png',
+  },
+]
+
+const Services = () => {
+  return <div></div>
+}
+
+export default Services
