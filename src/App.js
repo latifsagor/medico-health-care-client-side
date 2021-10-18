465b4e530abbc635882a5f500cdbafd2325f7d44
latifsagor <latif.sagor1042@gmail.com>
2021-10-18 14:38:10 +0600

    not found page create

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import logo from './logo.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './Components/Home/Home/Home'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import NotFound from './Components/NotFound/NotFound'
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello world</h2>
-      <Home></Home>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   )
 }
